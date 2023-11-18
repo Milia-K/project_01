4e2422853114ea46cb6f32251b9c66b3f5064ed9
Milia-K <milyamilya20@gmail.com>
2023-11-19 01:56:57 +0300

    first commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer(){
+    return(
+        <div className="Footer"> 
+            &#169; All Rights Reserved
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <div className='Header'>
+            <h1>my dictionary</h1>
+            <h2> It is never too late to learn </h2>
+        </div>
+    )
+}
+export default Header;
